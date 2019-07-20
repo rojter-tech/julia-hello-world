7bad5bc99b259af1ce92fbda02fb3c65a0751eec
Daniel Reuter <dreuter@rojter.local>
2019-07-20 21:04:48 +0200

    added some more stuff

--- src/strings.jl
+++ src/strings.jl
@@ -13,3 +13,23 @@ println("$number")
 stripstring = "   a a a a a a   "
 println(stripstring)
 println(strip(stripstring,[' ']))
+
+s1 = "abcπΣd"
+
+println("Size of s1: $(sizeof(s1))")
+println("Length of s1: $(length(s1))")
+
+hg = 80
+s2 = string("xy",3,Char(0x3B1),false,s1,hg)
+
+println("Size of s2: $(sizeof(s2))")
+println("Length of s2: $(length(s2))")
+
+s = "abcde"
+t = transcode(UInt8,s)
+
+a1 = ascii(s)
+#a2_f = ascii(s1) # yields error becaise it contains at least one invalid ascii
+a2 = transcode(UInt32, s1)
+
+happyface = Char(0x263A)
